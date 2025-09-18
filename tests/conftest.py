148d70f8f1a81369d9c6c6215d9aeb1290a4cfe8
Miguel <mloza693@gmail.com>
2025-09-17 23:11:16 -0400

    actualizando artifact3

--- tests/conftest.py
+++ tests/conftest.py
@@ -33,7 +33,7 @@ def driver(request):
         user_data_dir = tempfile.mkdtemp()
         chrome_options.add_argument(f'--user-data-dir={user_data_dir}')
 
-        #chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
 
